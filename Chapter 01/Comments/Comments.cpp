f2554e352d6137b73a5d5aad2760c6b1417aa576
kunallad-dev <kunallad062420@gmail.com>
2025-12-02 18:50:27 +0530

    Comments Cpp

---
+++ Chapter 01/Comments/Comments.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+void commentExample() {
+    std::cout << "This is an example of comments in C++." << std::endl;
+    // Single-line comment: The line above prints a message to the console.
+    /*
+         Multi-line comment:
+         The line above uses std::cout to output text.
+         This demonstrates how to use comments in C++ code.
+    */
+}
+
+int main() {
+    commentExample();
+    return 0;
+}
